8f9cd606b208765b32f8e53a65deb9fe37d599cf
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-02-29 12:38:09 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,12 @@
 
 const userName =document.querySelector("#name-input");
-const OutName =document.querySelector("#name-output");
+const outName =document.querySelector("#name-output");
 
 userName.addEventListener ("input",handleInput);
 function handleInput(event){
-const userName = event. currentTarget. value;
-const OutName=event. currentTarget. value ;
+    if (event.currentTarget.value.trim() === "") 
+    {outName.textContent = "Anonymous"}
 
-
-if (userName. type ==="text") {
-    
-alert (`Hello ${userName}`)
-} else {
-    userName. type ===" "
-   
-    alert (`Hello, ${OutName}`) 
+    else {outName.textContent = event.currentTarget.value.trim()}
 }
-}
-
-
 
