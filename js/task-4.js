a96e2c178ed1975dd79c4d5cec4d252c11a893ce
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-03-01 23:41:55 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,2 +1,17 @@
-const loginForm = document.querySelector('.login-form');
-loginForm.addEventListener("submit", handleSubmit);
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const elements = event.currentTarget.elements;
+  console.log(elements);
+  const info = {
+    email: elements.email.value,
+    password: elements.password.value,
+  };
+  if (info === "") {
+    alert("All form fields must be filled in");
+  } else {
+    console.log(info);
+  }
+  event.currentTarget.reset();
+}
