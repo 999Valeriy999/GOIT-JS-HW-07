55d2c484a31b3a19fbd51e293f3edb71609ab657
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-03-03 19:40:48 +0200

    task6

--- js/task-6.js
+++ js/task-6.js
@@ -1,15 +1,32 @@
-const Controls = document.querySelector("#controls");
-const Boxes = document.querySelector("#boxes");
 
-let rendomColor = getRandomHexColor();
-let boxWidth = parseInt(getComputedStyle(Controls).width);
-let boxHight = parseInt(getComputedStyle(Controls).height);
+const box = document.querySelector("#boxes");
+const btnDestroy = document.querySelector("data-destroy");
+const input = document.querySelector(input);
 
-function increaseFunc() {
-  boxWidth += 10;
-  boxHight += 10;
-  Controls.style.width = `${boxWidth}`;
-  Controls.style.height = `${boxHight}`;
+btnCreate .addEventListener("click",createBoxes);
+btnDestroy.addEventListener("click",destroyEl )
+input.addEventListener("input", createBoxes);
+const greate=input.value;
+const parsed = parseInt(greate, 10);
+if (parsed) {greate>0 = createBoxes};
+else {
+
+}
+
+btnCreate.addEventListener(`click`, createBoxes);
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    // Створення нового div елементу (блоку)
+
+    // Встановлення ширини та висоти блоку залежно від i
+    box.style.width = 30 + 10 * i + "px";
+    box.style.height = 30 + 10 * i + "px";
+    // Встановлення випадкового кольору фону для блоку за допомогою функції getRandomHexColor()
+    box.style.backgroundColor = getRandomHexColor();
+    // Додавання створеного блоку до елементу з id "boxes"
+    boxes.append(box);
+  }
 }
 
 function getRandomHexColor() {
@@ -17,3 +34,8 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+function destroyEl(parsed){
+divEl.InnerHtml="";
+}
+
+
