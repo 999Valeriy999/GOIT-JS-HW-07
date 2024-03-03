c186f90ae1efd9f9ede20c956771745e63e24580
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-03-04 00:55:51 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,41 +1,37 @@
-
-const box = document.querySelector("#boxes");
-const btnDestroy = document.querySelector("data-destroy");
-const input = document.querySelector(input);
-
-btnCreate .addEventListener("click",createBoxes);
-btnDestroy.addEventListener("click",destroyEl )
-input.addEventListener("input", createBoxes);
-const greate=input.value;
-const parsed = parseInt(greate, 10);
-if (parsed) {greate>0 = createBoxes};
-else {
-
-}
-
-btnCreate.addEventListener(`click`, createBoxes);
-
+const createBtn = document.querySelector("data-create"); // константа для кнопки create
+const box = document.querySelector("#boxes"); //константа для діва щоб створювало квадратик
+const btnDestroy = document.querySelector("data-destroy"); // константа для кнопки destroy
+createBtn.addEventListener("click", createBoxes); // слухач на кнопку create
 function createBoxes(amount) {
   for (let i = 0; i < amount; i++) {
     // Створення нового div елементу (блоку)
-
+    const box = document.createElement("div");
     // Встановлення ширини та висоти блоку залежно від i
     box.style.width = 30 + 10 * i + "px";
     box.style.height = 30 + 10 * i + "px";
     // Встановлення випадкового кольору фону для блоку за допомогою функції getRandomHexColor()
     box.style.backgroundColor = getRandomHexColor();
     // Додавання створеного блоку до елементу з id "boxes"
-    boxes.append(box);
+    document.body.append(box);
+  }
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  } /*функція зміни кольору*/
+  function createBoxes(event) {
+    event.preventDefault(); //функція щоб не перезавантажувалась сторінка
+  }
+  const greate = input.value; // константа для інпута
+  const parsed = parseInt(greate, 10); // константа щоб перетворити значення інпута на число
+  // перевірка інпута на коректне число
+  if (parsed > 0) {
+    createBoxes(parsed);
   }
 }
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+btnDestroy.addEventListener("click", destroyBox); //слухач на кнопку destroy
+// функція видалення
+function destroyBox() {
+  const btnDestroy = (divEl.innerHTML = " "); // видалення квадратиків
 }
-function destroyEl(parsed){
-divEl.InnerHtml="";
-}
-
-
