cbad60d38902aed80eba778886f32199a5f628c2
Valeriy Borodenko <143195865+999Valeriy999@users.noreply.github.com>
2024-03-05 19:46:38 +0200

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -1,6 +1,5 @@
 const colorButton = document.querySelector(".change-color");
 const colorSpan = document.querySelector(".color");
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
